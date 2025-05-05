aec0fdb1b908ad5b8faaadb96ba7e6cf4c0c4b2c
Sonal Mittal <sonalmittal140103@gmail.com>
2025-05-05 22:55:05 +0530

    Few inputs

--- Hello/Hello/settings.py
+++ Hello/Hello/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Hello.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "tempelates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Added Manually 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
--- Hello/home/views.py
+++ Hello/home/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is HomePage.")
+    # return HttpResponse("This is HomePage.")
+    context = {
+        'variable1' : "Sonal is amazing",
+        'variable2' : "Hehehehe"
+
+    }
+    return render(request, 'index.html', context)
     
 def about(request):
     return HttpResponse("This is About Page.")
